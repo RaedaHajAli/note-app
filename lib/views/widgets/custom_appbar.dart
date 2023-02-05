1de99f6ab3ac52662495ece8485547a96b953c94
Raedahajali <leen21mouhmad69@hotmail.com>
2023-02-05 10:32:40 +0200

    create custom app bar with custom search icon

---
+++ lib/views/widgets/custom_appbar.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:noteapp/views/widgets/custom_search_icon.dart';
+
+class CustomAppBar extends StatelessWidget {
+  const CustomAppBar({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: const [
+        Text(
+          'Notes',
+          style: TextStyle(fontSize: 28, color: Colors.white),
+        ),
+        CustomSearchIcon()
+      ],
+    );
+  }
+}
