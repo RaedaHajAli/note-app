c3b9d7b20c9e367b5ffbe7af406805700b8b290d
Raedahajali <leen21mouhmad69@hotmail.com>
2023-02-06 11:59:38 +0200

    generate note model adapter

--- lib/models/note_model.dart
+++ lib/models/note_model.dart
@@ -1,12 +1,21 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
-class NoteModel {
-  final String titile;
-  final String subTitile;
+import 'package:hive/hive.dart';
+
+part 'note_model.g.dart';
+
+@HiveType(typeId: 0)
+class NoteModel extends HiveObject {
+  @HiveField(0)
+  final String title;
+  @HiveField(1)
+  final String subTitle;
+  @HiveField(2)
   final String date;
+  @HiveField(3)
   final int color;
   NoteModel({
-    required this.titile,
-    required this.subTitile,
+    required this.title,
+    required this.subTitle,
     required this.date,
     required this.color,
   });
