b4ce6e3b167bcc0962eff1b090558fa9412ab56b
Raedahajali <leen21mouhmad69@hotmail.com>
2023-02-09 09:13:19 +0200

    change loading in bottom sheet, add absorbpointer

--- lib/views/widgets/add_note_form.dart
+++ lib/views/widgets/add_note_form.dart
@@ -45,22 +45,27 @@ class _AddNoteFormState extends State<AddNoteForm> {
           const SizedBox(
             height: 25,
           ),
-          CustomButton(
-            text: 'Add',
-            onTap: () {
-              if (formKey.currentState!.validate()) {
-                formKey.currentState!.save();
-                NoteModel note = NoteModel(
-                    title: title!,
-                    subTitle: content!,
-                    date: DateTime.now().toString(),
-                    color: Colors.orange.value);
-                BlocProvider.of<AddNoteCubit>(context).addNote(note);
-              } else {
-                autovalidateMode = AutovalidateMode.always;
-                setState(() {});
-              }
-            },
+          BlocBuilder<AddNoteCubit,AddNoteState>(
+            builder: (context,state) {
+              return CustomButton(
+                isLoading:state is AddNoteLoading?true:false,
+                text: 'Add',
+                onTap: () {
+                  if (formKey.currentState!.validate()) {
+                    formKey.currentState!.save();
+                    NoteModel note = NoteModel(
+                        title: title!,
+                        subTitle: content!,
+                        date: DateTime.now().toString(),
+                        color: Colors.orange.value);
+                    BlocProvider.of<AddNoteCubit>(context).addNote(note);
+                  } else {
+                    autovalidateMode = AutovalidateMode.always;
+                    setState(() {});
+                  }
+                },
+              );
+            }
           )
         ],
       ),
--- lib/views/widgets/custom_button.dart
+++ lib/views/widgets/custom_button.dart
@@ -1,12 +1,12 @@
-
 import 'package:flutter/material.dart';
 
 import '../../constants.dart';
 
 class CustomButton extends StatelessWidget {
-  CustomButton({this.onTap,required this.text});
-  String? text;
-  VoidCallback? onTap;
+  const CustomButton({this.onTap, required this.text, this.isLoading = false});
+  final String? text;
+  final VoidCallback? onTap;
+  final bool isLoading;
 
   @override
   Widget build(BuildContext context) {
@@ -17,8 +17,18 @@ class CustomButton extends StatelessWidget {
         decoration: BoxDecoration(
             color: kPrimaryColor, borderRadius: BorderRadius.circular(10)),
         alignment: Alignment.center,
-        child: Text('$text',
-            style:  TextStyle(fontSize: 24, color:Colors.black.withOpacity(0.8))),
+        child: isLoading
+            ? SizedBox(
+              height: 24,
+              width: 24,
+              child: CircularProgressIndicator(
+                color: Colors.black.withOpacity(0.8),
+                
+              ),
+            )
+            : Text('$text',
+                style: TextStyle(
+                    fontSize: 24, color: Colors.black.withOpacity(0.8))),
       ),
     );
   }
