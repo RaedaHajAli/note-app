815a455033f36930d9cdad7025f12b126472d741
Raedahajali <leen21mouhmad69@hotmail.com>
2023-02-09 10:05:05 +0200

    create notes cubit

---
+++ lib/cubits/notes_cubit/notes_state.dart
@@ -0,0 +1,21 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+part of 'notes_cubit.dart';
+
+@immutable
+abstract class NotesState {}
+
+class NotesInitial extends NotesState {}
+
+class NotesSuccess extends NotesState {
+final  List<NoteModel> notes;
+  NotesSuccess({
+    required this.notes,
+  });
+}
+
+class NotesFailure extends NotesState {
+  final String errorMessage;
+  NotesFailure({
+    required this.errorMessage,
+  });
+}
--- lib/main.dart
+++ lib/main.dart
@@ -2,22 +2,21 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 
-
 import 'package:noteapp/constants.dart';
-import 'package:noteapp/cubits/add_note_cubit/add_note_cubit.dart';
+import 'package:noteapp/cubits/notes_cubit/notes_cubit.dart';
+
 import 'package:noteapp/models/note_model.dart';
 import 'package:noteapp/simple_bloc_observer.dart';
 import 'package:noteapp/views/note_view.dart';
 
 void main() async {
   await Hive.initFlutter();
- Hive.registerAdapter(NoteModelAdapter());
- 
+  Hive.registerAdapter(NoteModelAdapter());
+
   await Hive.openBox<NoteModel>(kNotesBox);
 
- 
-   Bloc.observer = SimpleBlocObserver();
-  
+  Bloc.observer = SimpleBlocObserver();
+
   runApp(const NotesApp());
 }
 
@@ -26,10 +25,13 @@ class NotesApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(fontFamily: 'Itim', brightness: Brightness.dark),
-      home: const NotesView(),
+    return BlocProvider(
+      create: (context) => NotesCubit(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(fontFamily: 'Itim', brightness: Brightness.dark),
+        home: const NotesView(),
+      ),
     );
   }
 }
